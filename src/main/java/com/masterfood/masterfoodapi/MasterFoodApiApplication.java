c9e20dcb16a1431b62bc5d5cce140a932d14c7d4
Jaison Pereira <jaison_pereira@hotmail.com>
2019-02-21 03:36:51 -0300

    fist commit

---
+++ src/main/java/com/masterfood/masterfoodapi/MasterFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.masterfood.masterfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterFoodApiApplication.class, args);
+	}
+
+}
